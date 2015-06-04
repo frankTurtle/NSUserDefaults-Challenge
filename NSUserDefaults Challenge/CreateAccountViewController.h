0c93812c8746c0a5bb4d9294ba03aef92f23c3f2
Barret J. Nobel <bear.nobel@gmail.com>
2015-06-04 09:31:34 -0400

    added 2 viewControllers 1. SignInVC 2. CreateAccoutnVC

--- NSUserDefaults Challenge/CreateAccountViewController.h
+++ NSUserDefaults Challenge/CreateAccountViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  CreateAccountViewController.h
 //  NSUserDefaults Challenge
 //
 //  Created by OSX on 6/4/15.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface CreateAccountViewController : UIViewController
 
 @end
-
---
+++ NSUserDefaults Challenge/SignInViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignInViewController.h
+//  NSUserDefaults Challenge
+//
+//  Created by OSX on 6/4/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignInViewController : UIViewController
+
+@end
